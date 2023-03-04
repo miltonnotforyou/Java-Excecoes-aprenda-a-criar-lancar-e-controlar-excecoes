f0743e8de44a78072658ce00163526f5e64f32aa
Miltonnnotforyou <miltonnotforyou@yahoo.com.br>
2023-03-04 12:00:36 -0300

    Adicionando arquivos do curso

--- src/FluxoComTratamento.java
+++ src/FluxoComTratamento.java
@@ -1,4 +1,4 @@
-public class Fluxo {
+public class FluxoComTratamento {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
@@ -6,7 +6,7 @@ public class Fluxo {
         metodo1();
         } catch (ArithmeticException | NullPointerException ex) {
             String msg = ex.getMessage();
-            System.out.println("Exception" + msg);
+            System.out.println("Exception " + msg);
             ex.printStackTrace();
         }
 
@@ -21,13 +21,9 @@ public class Fluxo {
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-                int a = i / 0;
-//            Conta c = null;
-//            c.deposita();
 
-        }
-        System.out.println("Fim do metodo2");
+        throw new ArithmeticException("deu erro");
+
+//        System.out.println("Fim do metodo2");
     }
 }
